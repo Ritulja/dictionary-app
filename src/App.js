d8d58880311838d99e7ce40466a0570cba5a4dff
Ritulja <104376569+Ritulja@users.noreply.github.com>
2022-11-21 21:41:02 +0000

    Added footer

--- src/App.js
+++ src/App.js
@@ -11,7 +11,24 @@ function App() {
         </div>
       </header>
       <footer className="App-footer">
-        <small>Coded by Margareta Krajnikovics</small>
+        <small>
+          This project was coded by Margareta Krajnikovics and is{" "}
+          <a
+            href="https://github.com/Ritulja/dictionary-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://lovely-beignet-396d6d.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            hosted on Netlify
+          </a>
+        </small>
       </footer>
     </div>
   );
